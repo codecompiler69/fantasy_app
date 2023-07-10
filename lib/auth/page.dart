74cd523aab344363d341f0652636a5c56f18b8f4
codecompiler69 <tosarthak07@gmail.com>
2023-07-10 16:21:00 +0530

    bottom nav bar

--- lib/auth/page.dart
+++ lib/auth/page.dart
@@ -1,8 +1,10 @@
 import 'package:fantasyapp/screens/welcome_screen.dart';
-import 'package:fantasyapp/screens/home_screen.dart';
+
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../screens/main_page.dart';
 
 class CheckForSignIn extends StatelessWidget {
   const CheckForSignIn({super.key});
@@ -13,7 +15,7 @@ class CheckForSignIn extends StatelessWidget {
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return  HomeScreen();
+            return MainPage();
           } else {
             return const WelcomeScreen();
           }
